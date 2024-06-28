9edcdd198e387fe5c4ca37d469771541f779538e
JeffRisberg <jrisberg@aol.com>
2024-06-27 17:25:37 -0700

    added extra print statement

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
 
   public static void main(String[] args) {
+
+    System.out.println("Welcome");
+
     log.info("Welcome to Example 1");
   }
 }
