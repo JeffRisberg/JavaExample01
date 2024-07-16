c3e4c83d534d11aee6f783d88df25b283e22881a
JeffRisberg <jrisberg@aol.com>
2024-07-16 15:26:52 -0700

    moved python code into its own project

--- src/main/java/com/company/ExampleServlet.java
+++ src/main/java/com/company/ExampleServlet.java
@@ -1,16 +1,21 @@
 package com.company;
 
 import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.util.Enumeration;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
 public class ExampleServlet extends HttpServlet implements Filter {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
@@ -32,5 +37,27 @@ public class ExampleServlet extends HttpServlet implements Filter {
     response.addHeader("Access-Control-Allow-Origin", "*");
 
     chain.doFilter(servletRequest, response);
+  }
+
+  public void doFilter2(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest req = (HttpServletRequest) request;
+    Enumeration<String> params = req.getParameterNames();
+    while(params.hasMoreElements()){
+      String name = params.nextElement();
+      String value = request.getParameter(name);
+      log(req.getRemoteAddr() + "::Request Params::{"+name+"="+value+"}");
+    }
+
+    Cookie[] cookies = req.getCookies();
+    if(cookies != null){
+      for(Cookie cookie : cookies){
+        log(req.getRemoteAddr() + "::Cookie::{"+cookie.getName()+","+cookie.getValue()+"}");
+      }
+    }
+    
+    ((HttpRequest) req).addHeader("Access-Control-Allow-Origin", "*");
+
+    // pass the request along the filter chain
+    chain.doFilter(request, response);
   }
 }
